9a0ee8711d0e6f07e6cb2f75b53f545667f9f00e
Sergiu Frunză <109218899+sergiufrunza@users.noreply.github.com>
2025-03-11 17:00:10 +0200

    add alimbic

--- fastapi-application/core/models/base.py
+++ fastapi-application/core/models/base.py
@@ -12,7 +12,7 @@ class Base(DeclarativeBase):
     __abstract__ = True
 
     @declared_attr.directive
-    def __abstract__(cls) -> str:
+    def __tablename__(cls) -> str:
         return camel_case_to_snake_case(cls.__name__)
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
